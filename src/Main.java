ba4313c81c7eb9b20e94682c25091cab62936491
YAN VICTOR DA SILVA DE MORAIS <yan.morais@alunos.sc.senac.br>
2025-05-21 10:22:56 -0300

    yan2

--- src/Main.java
+++ src/Main.java
@@ -2,63 +2,79 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 
-        ArrayList<Usuario> listaUsuarios = new ArrayList<>();
-        ArrayList<Livro> acervo = new ArrayList<>();
+		ArrayList<Usuario> listaUsuarios = new ArrayList<>();
+		ArrayList<Livro> acervo = new ArrayList<>();
 
-        while (true) {
-            System.out.println("""
-                    === Biblioteca Piriri Parara ===
-                    Digite para:
-                    1 - Emprestar um livro - 
-                    2 - Devolver um livro -
-                    3 - Adicionar novo livro -
-                    4 - Adicionar novo usuario - ok
-                    5 - Remover livro - 
-                    6 - Remover usuario -
-                    7 - Alterar livro -
-                    8 - Alterar usuario -
-                    9 - Sair - ok
-                    """);
-            System.out.print("Informe a opção: ");
-            int temp = scan.nextInt();
-            scan.nextLine();
-            switch (temp) {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    System.out.println("- Adicionar novo usuario -");
-                    System.out.print("Informe o nome: ");
-                    String nome = scan.nextLine();
-                    System.out.print("Informe o cpf (apenas numeros): ");
-                    String cpf = scan.next();
-                    System.out.println();
-                    Usuario usuario = new Usuario(nome, cpf);
-                    listaUsuarios.add(usuario);
-                    break;
-                case 9:
-                    System.exit(0);
-                default:
-                    System.out.println("Opção Invalida");
-                    break;
-                    
-                case 6:
-                	System.out.println ("- Remover usuario -");
-                	System.out.println ("Informe o CPf do usuario");
-                	String cpf2 = scan.next();
-                	for (Usuario u: listaUsuarios) {
-                		if (u.getCpf().equals(cpf2)) {
-                			listaUsuarios.removeIf(item -> item.equals(cpf2)); 
-           			
-                		}
-            }
-        }
-        }
-    }
+		while (true) {
+			System.out.println("""
+					=== Biblioteca Piriri Parara ===
+					Digite para:
+					1 - Emprestar um livro -
+					2 - Devolver um livro -
+					3 - Adicionar novo livro -
+					4 - Adicionar novo usuario - ok
+					5 - Remover livro -
+					6 - Remover usuario -
+					7 - Alterar livro -
+					8 - Alterar usuario -
+					9 - Sair - ok
+					""");
+			System.out.print("Informe a opção: ");
+			int temp = scan.nextInt();
+			scan.nextLine();
+			switch (temp) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				System.out.println("- Adicionar novo usuario -");
+				System.out.print("Informe o nome: ");
+				String nome = scan.nextLine();
+				System.out.print("Informe o cpf (apenas numeros): ");
+				String cpf = scan.next();
+				System.out.println();
+				Usuario usuario = new Usuario(nome, cpf);
+				listaUsuarios.add(usuario);
+				break;
+			case 9:
+				System.exit(0);
+			default:
+				System.out.println("Opção Invalida");
+				break;
+
+			case 6:
+				System.out.println("- Remover usuario -");
+				System.out.println("Informe o CPf do usuario");
+				String cpf2 = scan.next();
+				for (Usuario u : listaUsuarios) {
+					if (u.getCpf().equals(cpf2)) {
+						listaUsuarios.removeIf(item -> item.equals(cpf2));
+					}
+				}
+				System.out.println("Usuario removido");
+				break;
+				
+			case 5:
+				System.out.println("Remover um livro");
+				System.out.println("Informe o isbn do livro");
+				String isbn1 = scan.next();
+					for (Livro y : acervo) {
+						if (y.getIsbn().equals((isbn1))) {
+							acervo.removeIf(item -> item.equals(isbn1));
+						}
+					}
+					System.out.println("Livro removido");
+					break;
+					
+					
+					
+			}
+		}
+	}
 }
